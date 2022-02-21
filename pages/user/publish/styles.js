d8de48289a0f62060d9d5c4de21eea4e7fe3489a
Matheus F. Ribeiro <matheusfribeiro@outlook.com>
2022-02-21 18:31:41 -0300

    publish page refactor - styles, form values

---
+++ pages/user/publish/styles.js
@@ -0,0 +1,19 @@
+import { makeStyles } from '@material-ui/core/styles'
+
+
+const useStyles = makeStyles((theme) => ({
+  boxContainer: {
+    paddingBottom: theme.spacing(3),
+  },
+  box: {
+    backgroundColor: theme.palette.background.white,
+    padding: theme.spacing(3),
+    boxShadow: '2px 2px 2px gray',
+  },
+  inputLabel: {
+    fontWeight: 400,
+    color: theme.palette.primary.main
+  },
+}))
+
+export default useStyles
